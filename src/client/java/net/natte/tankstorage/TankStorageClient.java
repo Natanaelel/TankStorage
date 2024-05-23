b053b7aa535097ca061342416a63c824e9f1f4e0
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2024-05-23 14:44:27 +0200

    init

---
+++ src/client/java/net/natte/tankstorage/TankStorageClient.java
@@ -0,0 +1,10 @@
+package net.natte.tankstorage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TankStorageClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
